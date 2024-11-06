06666fba747f916bf66004143e0d5663f9eb2226
Baptiste <127935960+BaptisteLeRandom@users.noreply.github.com>
2024-11-06 16:18:07 +0100

    renderPage

--- festivalNRV/out/production/festivalNRV/iutnc/nrv/dispatch/Dispatcher.php
+++ festivalNRV/out/production/festivalNRV/iutnc/nrv/dispatch/Dispatcher.php
@@ -27,7 +27,27 @@ class Dispatcher
 
     public function renderPage ($html)
     {
-        echo $html;
+        echo <<<HTML
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Festival NRV</title>
+    </head>
+    <header>
+    <h1>Festival NRV</h1>
+    </header>
+    
+    <main>
+    $html
+    </main>
+    
+</html>
+
+HTML;
+
     }
 
 }
